698bf065366a1ce92b237014e0b30603c14292a4
LudoHoff <ludo.hoffstetter@epfl.ch>
2020-10-22 02:00:44 +0200

    Modify SimpleNet paths

--- SimpleNet/dataloader.py
+++ SimpleNet/dataloader.py
@@ -17,14 +17,14 @@ class SaliconDataset(DataLoader):
             transforms.ToTensor(),
             transforms.Normalize([0.5, 0.5, 0.5],
                                 [0.5, 0.5, 0.5])
-        ])    
-        
+        ])
+
     def __getitem__(self, idx):
         img_id = self.img_ids[idx]
-        img_path = os.path.join(self.img_dir, img_id + self.exten)
+        img_path = os.path.join(self.img_dir, img_id + '.jpg')
         gt_path = os.path.join(self.gt_dir, img_id + self.exten)
         fix_path = os.path.join(self.fix_dir, img_id + self.exten)
-        
+
         img = Image.open(img_path).convert('RGB')
 
         gt = np.array(Image.open(gt_path).convert('L'))
@@ -33,18 +33,18 @@ class SaliconDataset(DataLoader):
 
         fixations = np.array(Image.open(fix_path).convert('L'))
         fixations = fixations.astype('float')
-        
+
         img = self.img_transform(img)
         if np.max(gt) > 1.0:
             gt = gt / 255.0
         fixations = (fixations > 0.5).astype('float')
-        
+
         assert np.min(gt)>=0.0 and np.max(gt)<=1.0
         assert np.min(fixations)==0.0 and np.max(fixations)==1.0
         return img, torch.FloatTensor(gt), torch.FloatTensor(fixations)
-    
+
     def __len__(self):
-        return len(self.img_ids)    
+        return len(self.img_ids)
 
 class TestLoader(DataLoader):
     def __init__(self, img_dir, img_ids):
@@ -55,8 +55,8 @@ class TestLoader(DataLoader):
             transforms.ToTensor(),
             transforms.Normalize([0.5, 0.5, 0.5],
                                 [0.5, 0.5, 0.5])
-        ])    
-        
+        ])
+
     def __getitem__(self, idx):
         img_id = self.img_ids[idx]
         img_path = os.path.join(self.img_dir, img_id)
@@ -64,7 +64,7 @@ class TestLoader(DataLoader):
         sz = img.size
         img = self.img_transform(img)
         return img, img_id, sz
-    
+
     def __len__(self):
         return len(self.img_ids)
 
@@ -82,13 +82,13 @@ class MITDataset(DataLoader):
             transforms.Normalize([0.5, 0.5, 0.5],
                                 [0.5, 0.5, 0.5])
         ])
-        
+
     def __getitem__(self, idx):
         img_id = self.img_ids[idx]
         img_path = os.path.join(self.img_dir, img_id + '.png')
         gt_path = os.path.join(self.gt_dir, img_id + self.exten)
         fix_path = os.path.join(self.fix_dir, img_id + self.exten)
-        
+
         img = Image.open(img_path).convert('RGB')
 
         gt = np.array(Image.open(gt_path).convert('L'))
@@ -97,12 +97,12 @@ class MITDataset(DataLoader):
 
         fixations = np.array(Image.open(fix_path).convert('L'))
         fixations = fixations.astype('float')
-        
+
         img = self.img_transform(img)
         if np.max(gt) > 1.0:
             gt = gt / 255.0
         fixations = (fixations > 0.5).astype('float')
-        
+
         assert np.min(gt)>=0.0 and np.max(gt)<=1.0
         assert np.min(fixations)==0.0 and np.max(fixations)==1.0
         if self.val:
@@ -110,6 +110,6 @@ class MITDataset(DataLoader):
         else:
             return img, torch.FloatTensor(gt), torch.FloatTensor(gt)
 
-    
+
     def __len__(self):
-        return len(self.img_ids)    
+        return len(self.img_ids)
