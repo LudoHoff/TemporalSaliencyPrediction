c28822baee24b5dc57936801d835f8aad673257e
LudoHoff <ludo.hoffstetter@epfl.ch>
2020-11-27 01:03:55 +0100

    Fix torch code

--- src/generate_saliency_volumes.py
+++ src/generate_saliency_volumes.py
@@ -15,7 +15,7 @@ class GaussianBlur1D(nn.Module):
         super(GaussianBlur1D, self).__init__()
         self.size = 17
         kernel = gauss(self.size, 2)
-        kernel = torch.FloatTensor(kernel).unsqueeze(0)
+        kernel = torch.cuda.HalfTensor(kernel).unsqueeze(0)
         self.weight = nn.Parameter(data=kernel, requires_grad=False)
  
     def forward(self, x):
@@ -27,7 +27,7 @@ class GaussianBlur2D(nn.Module):
         super(GaussianBlur2D, self).__init__()
         self.size = 101
         kernel = np.outer(gauss(self.size, 25))
-        kernel = torch.cuda.tensor(kernel, dtype=torch.float16).unsqueeze(0).unsqueeze(0)
+        kernel = torch.from_numpy(kernel, dtype=torch.float16).unsqueeze(0).unsqueeze(0).cuda()
         self.weight = nn.Parameter(data=kernel, requires_grad=False)
  
     def forward(self, x):
@@ -37,13 +37,13 @@ class GaussianBlur2D(nn.Module):
 filenames = get_filenames(FIXATION_PATH + TRAIN_PATH)[0:2]
 
 print("Parsing fixations...")
-saliency_volumes = torch.from_numpy(get_saliency_volumes(filenames, progress_bar=True))
+saliency_volumes = get_saliency_volumes(filenames, progress_bar=True)
 
 print("Generating saliency volumes...")
-temporal_maps = torch.cuda.zeros([len(filenames),25,H,W], dtype=torch.float16)
+temporal_maps = torch.zeros([len(filenames),25,H,W], dtype=torch.float16).cuda()
 
-conv1D = GaussianBlur1D()
-conv2D = GaussianBlur2D()
+conv1D = GaussianBlur1D().cuda()
+conv2D = GaussianBlur2D().cuda()
 
 for i, saliency_volume in enumerate(tqdm(saliency_volumes)):
     fix_timestamps = sorted([fixation for fix_timestamps in saliency_volume
@@ -60,7 +60,7 @@ for i, saliency_volume in enumerate(tqdm(saliency_volumes)):
 
     for x in range(W):
         for y in range(H):
-            temporal_maps[i,:,y,x] = conv2D.forward(temporal_maps[i,:,y,x])
+            temporal_maps[i,:,y,x] = conv2D(temporal_maps[i,:,y,x])
             #temporal_maps[i,:,y,x] = gpu.convolve(temporal_maps[i,:,y,x], kernel1D)
             #ndimage.gaussian_filter1d(temporal_maps[i,:,y,x], 2, 0)
 
