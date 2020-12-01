b113df02f18b8b259653a95b0a278962052fa9f8
Hoffstetter Ludo <hoffstet@iccluster135.iccluster.epfl.ch>
2020-12-01 14:12:43 +0100

    Working saliency volume generator

--- SimpleNet/generate_saliency_volumes.py
+++ SimpleNet/generate_saliency_volumes.py
@@ -3,20 +3,26 @@ import cv2
 
 def generate_fixation_files(path, time_slices):
     filenames = [nm.split(".")[0] for nm in os.listdir(PARS_FIX_PATH + path)]
-    write_path = SAL_VOL_PATH + time_slices + '/' + path
-    os.mkdir(write_path)
+    
+    write_path = SAL_VOL_PATH + str(time_slices)
+    if not os.path.exists(write_path):
+        os.makedirs(write_path)
+
+    write_path = write_path + '/' + path
+    if not os.path.exists(write_path):
+        os.makedirs(write_path)
 
     conv1D = GaussianBlur1D(time_slices).cuda()
     conv2D = GaussianBlur2D().cuda()
 
     for filename in tqdm(filenames):
-        fixation_volume = np.load(PARS_FIX_PATH + filename + '.npy', allow_pickle=True)
+        fixation_volume = np.load(PARS_FIX_PATH + path  + filename + '.npy', allow_pickle=True)
         saliency_volume = get_saliency_volume(fixation_volume, conv1D, conv2D, time_slices)
         saliency_volume = np.swapaxes(saliency_volume.squeeze(0).squeeze(0).detach().cpu().numpy(), 0, -1)
         saliency_volume = np.swapaxes(cv2.resize(saliency_volume, (256,256)), 0, -1)
 
         for i, saliency_slice in enumerate(saliency_volume):
-            cv2.imwrite(write_path + filename + '_' + i + '.png', 255 * saliency_slice / saliency_slice.max())
+            cv2.imwrite(write_path + filename + '_' + str(i) + '.png', 255 * saliency_slice / saliency_slice.max())
 
 generate_fixation_files('train/', 10)
 generate_fixation_files('val/', 10)
