fbf9c1cee97bd1895cff84eb436424650c9a974b
LudoHoff <ludo.hoffstetter@epfl.ch>
2020-12-16 20:11:01 +0100

    SIMPLE NET

--- SimpleNet/validate_volumes.py
+++ SimpleNet/validate_volumes.py
@@ -17,7 +17,6 @@ from matplotlib.image import imread
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--model_val_path',default="model.pt", type=str)
-parser.add_argument('--results_dir',default="volume_predictions_10/val/", type=str)
 parser.add_argument('--time_slices',default=10, type=int)
 parser.add_argument('--samples',default=50, type=int)
 parser.add_argument('--no_workers',default=4, type=int)
@@ -45,7 +44,8 @@ val_img_dir = args.dataset_dir + "images/val/"
 val_gt_dir = args.dataset_dir + "maps/val/"
 val_fix_dir = args.dataset_dir + "fixation_maps/val/"
 val_vol_dir = args.dataset_dir + "saliency_volumes_" + str(args.time_slices) + "/val/"
-val_pred_dir = args.dataset_dir + args.results_dir
+val_pred_dir = args.dataset_dir + "volumes/predictions/"
+val_vol_gt_dir = args.dataset_dir + "volumes/ground_truths/"
 
 val_img_ids = [nm.split(".")[0] for nm in os.listdir(val_img_dir)]
 val_dataset = SaliconVolDataset(val_img_dir, val_gt_dir, val_fix_dir, val_vol_dir, val_img_ids, args.time_slices)
@@ -54,6 +54,7 @@ val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=Fals
 with torch.no_grad():
     model.eval()
     os.makedirs(val_pred_dir, exist_ok=True)
+    os.makedirs(val_vol_gt_dir, exist_ok=True)
 
     kl_avg = torch.FloatTensor([0.0]).to(device)
     cc_avg = torch.FloatTensor([0.0]).to(device)
@@ -92,10 +93,10 @@ with torch.no_grad():
             img = imread(img_path)
 
             anim1 = animate(pred_vol, img, False)
-            anim1.save(args.dataset_dir + args.results_dir + str(i) + '_predicted_volume.gif', writer=animation.PillowWriter(fps=10))
+            anim1.save(val_pred_dir + val_img_ids[i] + '.gif', writer=animation.PillowWriter(fps=10))
 
             anim2 = animate(vol, img, False)
-            anim2.save(args.dataset_dir + args.results_dir + str(i) + '_ground_truth_volume.gif', writer=animation.PillowWriter(fps=10))
+            anim2.save(val_vol_gt_dir + val_img_ids[i] + '.gif', writer=animation.PillowWriter(fps=10))
 
             plt.close('all')
 
